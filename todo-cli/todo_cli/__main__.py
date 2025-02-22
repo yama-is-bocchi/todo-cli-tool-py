fbcc24336ed0383629070ef3ccdb6714fa692f48
yamaa <yamaa.com987654321@outlook.jp>
2025-02-22 12:11:01 +0000

    パーサー追加

--- todo-cli/todo_cli/__main__.py
+++ todo-cli/todo_cli/__main__.py
@@ -1,8 +1,13 @@
-def hello(name: str):
-    print(f"Hello {name}!")
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Todo CLI Tool")
+    parser.add_argument("command", choices=[
+                        "create", "list", "edit", "delete"], help="Command to execute")
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == "__main__":
-    import jsonargparse 
-
-    jsonargparse.CLI()
+    main()
